ff2c5670518cfd71446793d1506407ac7f6ae842
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-09-22 11:54:06 +0530

    style: remove unnecessary whitespace in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+// import Nav from './Components/Nav'
 
 function App() {
   
 
   return (
     <>
-      <Header />
+      <Header/>
       <Outlet />
       <Footer />
     </>
